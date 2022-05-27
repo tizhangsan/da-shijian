3800c63e678f02fcce67c2cf86ac56c6ea04bcc2
zhangzhang <1151398873@qq.com>
2022-05-27 21:49:45 +0800

    ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,48 @@
 
-    let urll= 'http://www.liulongbin.top:3007'
-     asdf()
+let urll = 'http://www.liulongbin.top:3007'
+asdf()
+//退出   
+$('#aa').click(() => {
+    layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function (index) {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
 
-   //退出   
-   $('#aa').click(()=>{
-    layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
-       localStorage.removeItem('token')
-       location.href= '/login.html'
-        
         layer.close(index);
-      });
+    });
 
-   })
-
-
- function asdf(){
-    let urll= 'http://www.liulongbin.top:3007'
+})
+function asdf() {
+    let urll = 'http://www.liulongbin.top:3007'
     $.ajax({
-        method : 'GET',
-        url :urll+'/my/userinfo',
+        method: 'GET',
+        url: urll + '/my/userinfo',
         headers: {
-            Authorization: localStorage.getItem('token')||''
+            Authorization: localStorage.getItem('token') || ''
         },
-        success: (res)=>{
-           
-             if(res.status !==0){
-                 return layer.msg(res.message)
-             }
-             if (res.data.user_pic===null){
-                 $('.pic').hide()
-             }else{
-                 $('.span').hide()
-                 $('.pic').attr('src',res.data.user_pic)
- 
-             }
-      
-             let name = res.data.nickname || res.data.username 
-             $('.i').html(`欢迎${name}`)
- 
+        success: (res) => {
+
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            if (res.data.user_pic === null) {
+                $('.pic').hide()
+            } else {
+                $('.span').hide()
+                $('.pic').attr('src', res.data.user_pic)
+
+            }
+
+            let name = res.data.nickname || res.data.username
+            $('.i').html(`欢迎${name}`)
+
         },
-        complete: (res)=>{
-        
-         if(res.responseJSON.status===1&&res.responseJSON.message==="身份认证失败！"){
-                 localStorage.removeItem('token')
-                 location.href='/login.html'
-         }
+        complete: (res) => {
+            //全局的一个bug
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
         }
-
-
-       
-   
-     
-     
     })
- }
+}
 
